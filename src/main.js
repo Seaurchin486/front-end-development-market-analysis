268c4ffd494d99b08d49278c50e530fab4e05f7d
海胆 <8065350+seaurchin486@user.noreply.gitee.com>
2022-09-27 23:21:06 +0800

    地图控件摸索清楚

--- src/main.js
+++ src/main.js
@@ -5,29 +5,38 @@ import App from './App.vue'
 import router from './route/route'
 import store from './store/store'
 
+
 import dataV from '@kjgl77/datav-vue3'
 import ECharts from 'vue-echarts'
-import { use } from "echarts/core"
+import { use,  registerMap } from "echarts/core"
+
 
 import {
   CanvasRenderer
 } from 'echarts/renderers'
 import {
-  BarChart
+  BarChart,
+  MapChart
 } from 'echarts/charts'
 import {
   GridComponent,
   TooltipComponent,
-  TitleComponent
+  TitleComponent,
+  VisualMapComponent
 } from 'echarts/components'
 
 use([
   CanvasRenderer,
   BarChart,
+  MapChart,
   GridComponent,
   TooltipComponent,
-  TitleComponent
+  TitleComponent,
+  VisualMapComponent
 ])
+
+import cherryJSON from './assets/map/hangzhou.json'
+registerMap('hangzhou', cherryJSON) 
 
 const app = createApp(App)
 app.component('v-echart', ECharts)
--- src/route/views/AreaDistribution.vue
+++ src/route/views/AreaDistribution.vue
@@ -2,13 +2,63 @@
 // 从 store 拿数
 // 根据地区计算总的
 // 做图表输出
-
+let option = {
+  tooltip: {
+    trigger: "item",
+  },
+  visualMap: {
+    left: "right",
+    min: 0,
+    max: 100,
+    inRange: {
+      color: [
+        "#313695",
+        "#4575b4",
+        "#74add1",
+        "#abd9e9",
+        "#e0f3f8",
+        "#ffffbf",
+        "#fee090",
+        "#fdae61",
+        "#f46d43",
+        "#d73027",
+        "#a50026",
+      ],
+    },
+  },
+  series: [
+    {
+      name: "hangzhou",
+      type: "map", //type必须声明为 map 说明该图标为echarts 中map类型
+      map: "hangzhou",
+      itemStyle: {
+        normal: {
+          label: {
+            show: true,
+            color: "#eaeaea",
+          },
+          borderWidth: 1, //边际线大小
+          borderColor: "#F5F5F5", //边界线颜色
+          areaColor: "#1F54FF", //默认区域颜色
+        },
+        emphasis: {
+          label: {
+            show: true,
+          },
+        },
+      },
+      data: [
+        {name: '上城区', value: 50}
+      ],
+    },
+  ],
+};
 </script>
     
 <template>
   <p>地区分布</p>
   <div class="content">
-    地图
+    <v-echart :option="option" />
   </div>
 </template>
     
